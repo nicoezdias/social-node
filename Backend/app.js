5df2b8152f5da000d4d06356d4822e6fc3cdf999
nicoezdias <nicolasezdias@gmail.com>
2023-02-17 17:16:57 -0300

    Add models

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed.routes');
 
 const app = express();
+mongoose.set('strictQuery', false);
+dotenv.config();
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
@@ -19,4 +24,9 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+	.connect(process.env.DB_URL)
+	.then((result) => {
+		app.listen(PORT);
+	})
+	.catch((err) => console.log(err));
