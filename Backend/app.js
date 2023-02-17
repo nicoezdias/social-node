547f5c7a145cc2a01154bbef80e5ea1b8b0ed2d1
nicoezdias <nicolasezdias@gmail.com>
2023-02-17 18:46:44 -0300

    Uploading images

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed.routes');
 
@@ -12,7 +13,31 @@ mongoose.set('strictQuery', false);
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		cb(null, new Date().toISOString() + '-' + file.originalname);
+	},
+});
+
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
 app.use(bodyParser.json());
+app.use(
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
